88f656ac0feb2089a9334b9570691516e8885a05
akshayrangnekar <akshay@annkh.com>
2016-03-17 16:00:24 +0000

    Wildcard path

--- server.js
+++ server.js
@@ -2,16 +2,33 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8081;
 
+if (!String.prototype.endsWith) {
+    String.prototype.endsWith = function(searchString, position) {
+        var subjectString = this.toString();
+        if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+            position = subjectString.length;
+        }
+        position -= searchString.length;
+        var lastIndex = subjectString.indexOf(searchString, position);
+        return lastIndex !== -1 && lastIndex === position;
+    };
+}
+
 var aws = require('knox').createClient({
     key: process.env.AWS_ACCESS_KEY,
     secret: process.env.AWS_ACCESS_KEY_SECRET,
     bucket: process.env.BUCKET_NAME
-})
-
-app.get('/:path', function (req, res) {
-    res.send('Hello World: ' + req.params.path);
 });
 
-app.listen(process.env.PORT, function () {
+var handler = function(req, res) {
+    var path = req.path;
+    res.send('Hello World: ***' + path + '***');
+
+};
+
+app.get('/', handler);
+app.get('/*', handler);
+
+app.listen(process.env.PORT, function() {
     console.log('Example app listening on port: ' + port);
 });
