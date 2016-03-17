91608c2d3a2ca909780640e6e123cccf52220160
akshayrangnekar <akshay@annkh.com>
2016-03-17 16:34:36 +0000

    Switch to S3 Streams

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var S3 = require('aws-sdk').S3,
+    S3S = require('s3-streams');
+
 var app = express();
 var port = process.env.PORT || 8081;
 
@@ -14,43 +17,19 @@ if (!String.prototype.endsWith) {
     };
 }
 
-var aws = require('knox').createClient({
-    key: process.env.AWS_ACCESS_KEY,
-    secret: process.env.AWS_ACCESS_KEY_SECRET,
-    bucket: process.env.BUCKET_NAME
-});
-
 var handler = function(req, res, next) {
     var path = '/' + req.hostname + req.path;
     if (path.endsWith('/')) path = path + 'index.html';
 
     // res.send('Hello World: ***' + path + '*** <br />Hostname: ' + req.hostname);
     console.log('Requesting path: ' + path);
-    aws.get(path)
-        .on('error', function(err) {
-            console.log('Error: ', err);
-            next(err);
-        })
-        .on('response', function(resp) {
-            console.log('Got response.');
-            if (resp.statusCode !== 200) {
-                var err = new Error()
-                err.status = 404
-                next(err)
-                return
-            }
 
-            res.setHeader('Content-Length', resp.headers['content-length'])
-            res.setHeader('Content-Type', resp.headers['content-type'])
+    var download = S3S.ReadStream(new S3(), {
+        Bucket: process.env.BUCKET_NAME,
+        Key: path
+    });
 
-            if (req.method === 'HEAD') {
-                res.statusCode = 200
-                res.end()
-                return
-            }
-
-            resp.pipe(res)
-        })
+    download.pipe(res);
 };
 
 app.get('/', handler);
