9bd9462d916a694a21b036f59bb9ca9ab3b7b9b4
akshayrangnekar <akshay@annkh.com>
2016-03-17 15:39:24 +0000

    Add Knox

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8081;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var aws = require('knox').createClient({
+    bucket: 'cardform-hosted-sites'
+})
+
+app.get('/:path', function (req, res) {
+    res.send('Hello World: ' + req.params.path);
 });
 
 app.listen(process.env.PORT, function () {
-  console.log('Example app listening on port: ' + port);
+    console.log('Example app listening on port: ' + port);
 });
