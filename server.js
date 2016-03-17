202cf3f61c90085de62a902f4da2cfefefe8e32e
akshayrangnekar <akshay@annkh.com>
2016-03-17 15:31:23 +0000

    Updated port

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8081;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT, function () {
+  console.log('Example app listening on port: ' + );
 });
